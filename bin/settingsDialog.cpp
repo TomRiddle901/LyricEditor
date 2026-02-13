16ea281a05e1a6eead1b263c766ec14f81100431
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-13 13:31:26 +0100

    Implemented SettingsDialog constructor with color and font selection buttons, layout setup, and signal-slot connections.

--- bin/settingsDialog.cpp
+++ bin/settingsDialog.cpp
@@ -5,3 +5,21 @@
 #include <QRect>
 #include <QRegularExpression>
 #include <QRegularExpressionMatch>
+
+SettingsDialog::SettingsDialog(QWidget *parent) : QDialog(parent), selectedColor(Qt::black),
+backgroundPwColor(Qt::black), selectedFont(QFont("Ubuntu", 12)){
+    btnChooseColor = new QPushButton("Scegli colore tema", this);
+    btnChooseFont = new QPushButton("Scegli Font", this);
+    lblFont = new QLabel("Font corrente: " + selectedFont.toString(), this);
+
+    QVboxLayout *settingsLayout = new QVBoxLayout(this);
+    settingLayout->addWidget(btnChooseColor);
+    settingLayout->addWidget(btnChooseFont);
+    settingLayout->addWidget(lblFont);
+
+    connect(btnChooseColor, &QPushButton::clicked, this, &SettingsDialog::chooseColor);
+    connect(btnChooseFont, &QPushButton::clicked, this, &SettingsDialog::chooseFont);
+
+    setWindowTitle("Lyric Editor Beta - Impostazioni");
+    setMinimumSize(600, 100);
+}
