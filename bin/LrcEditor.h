a6d913561a744ff8873d4e6276f07c16b48eb2de
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-13 13:19:30 +0100

    Added LrcEditor QWidget with media controls, slider seeking, LRC load/save, settings integration, and playback slots.

--- bin/LrcEditor.h
+++ bin/LrcEditor.h
@@ -21,3 +21,50 @@
 #include <QMainWindow>
 #include <QRegularExpression>
 #include <QRegularExpressionMatch>
+
+class LrcEditor : public QWidget{
+    Q_OBJECT
+
+    public:
+        explicit LrcEditor(QWidget *parent = nullptr);
+
+    private slots:
+        void openAudioFile();
+        void togglePlayPause();
+        void insertTimestampedText();
+        void updateTimerLabel(quint64 position);
+        void saveLyrictoFile();
+        void updateSlider(quint64 position);
+        void updateDuration(quint64 duration);
+        void seek();
+        void closeEvent(QCloseEvent *event) override;
+        void removeAudioFile();
+        void loadLrcfile();
+        void applicaImpostazioni(const QColor &colore, const QFont &font);
+
+    private:
+        QMediaPlayer *player;
+        QPushButton *openButton;
+        QPushButton *playPauseButton;
+        QPushButton *insertTextButton;
+        QPushButton *saveButton;
+        QPushButton *removeAudioButton;
+        QPushButton *loadLrcButton;
+        QPushButton *settingsButton;
+
+        QTextEdit *lyricsEditor;
+        
+        QLabel *nowPlaying;
+        QLabel *timerLabel;
+        
+        QSlider *positionSlider;
+
+        QString loadedLrcPath;
+
+        SettingsDialog *settingsDialog;
+        
+        bool isSliderPressed = false;
+        bool lrcLoaded = false;
+};
+
+#endif // LRCEDITOR_H
