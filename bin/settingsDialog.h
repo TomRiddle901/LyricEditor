fcc804f04efd8fefb98ecc2fe49c129a8ed468ea
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-13 13:25:19 +0100

    Added SettingsDialog QDialog for theme/color and font selection with change notification signal.

--- bin/settingsDialog.h
+++ bin/settingsDialog.h
@@ -9,3 +9,36 @@
 #include <QRect>
 #include <QRegularExpression>
 #include <QRegularExpressionMatch>
+
+class SettingsDialog : public QDialog{
+    Q_OBJECT
+
+    public:
+        explicit SettingsDialog(Qwidget *parent = nullptr);
+        ~SettingsDialog();
+
+        QColor getTheme() const;
+        QFont getFont() const;
+        QColor getBackgroundPwColor() const;
+
+    private slots:
+        void chooseColor();
+        coid chooseFont();
+
+    private:
+        QPushButton *btnChooseColor;
+        QPushButton *btnChooseFont;
+        QPushButton *btnChoosePwColor;
+
+        QLabel *lblFont;
+
+        QColor selectedColor;
+        QColor backgroundPwColor;
+
+        QFont selectedFont;
+
+    signals:
+        void impostazioniCambiate(const QColor &colore, const QFont &font);
+};
+
+#endif // SETTINGSDIALOG_H
